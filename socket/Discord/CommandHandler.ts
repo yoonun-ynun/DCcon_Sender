4abbd67053b90f7a3136a7ac5dafccd84381aaa8
yoonun <yoontkyoonun@gmail.com>
2025-12-12 17:32:58 +0000

    Fixed: Update to nextJS 15.5.9

--- socket/Discord/CommandHandler.ts
+++ socket/Discord/CommandHandler.ts
@@ -21,75 +21,7 @@ export async function handler(message: Message) {
         );
     }
     if (message.t === 'READY') {
-        createGlobalCommand({
-            name: 'list',
-            description: '추가한 디시콘 목록을 확인합니다.',
-            type: 1,
-        })
-            .then((res) => {
-                if (res?.ok !== true) {
-                    console.log(res?.message);
-                    return;
-                }
-                console.log('명령어 설정 완료');
-            })
-            .catch(() => console.error('명령어 설정에 실패하였습니다.'));
-        createGlobalCommand({
-            name: 'select',
-            description: '특정 디시콘을 idx, selector를 사용해서 전송합니다.',
-            type: 1,
-            options: [
-                {
-                    type: 3,
-                    name: 'idx',
-                    description: '해당 디시콘의 idx 값을 넣어주세요',
-                },
-                {
-                    type: 4,
-                    name: 'selector',
-                    description: '몇번째 콘을 가져올건지 입력 해 주세요',
-                    min_value: 1,
-                    max_value: 100,
-                },
-            ],
-        })
-            .then((res) => {
-                if (res?.ok !== true) {
-                    console.log(res?.message);
-                    return;
-                }
-                console.log('명령어 설정 완료');
-            })
-            .catch(() => console.error('명령어 설정에 실패하였습니다.'));
-        createGlobalCommand({
-            name: 'profile',
-            description: '프로필에 추가한 디시콘을 index, selector를 사용해서 전송합니다.',
-            type: 1,
-            options: [
-                {
-                    type: 4,
-                    name: 'index',
-                    description: '몇번째 디시콘을 선택할건지 입력 해 주세요',
-                    min_value: 1,
-                    max_value: 25,
-                },
-                {
-                    type: 4,
-                    name: 'selector',
-                    description: '몇번째 콘을 가져올건지 입력 해 주세요',
-                    min_value: 1,
-                    max_value: 100,
-                },
-            ],
-        })
-            .then((res) => {
-                if (res?.ok !== true) {
-                    console.log(res?.message);
-                    return;
-                }
-                console.log('명령어 설정 완료');
-            })
-            .catch(() => console.error('명령어 설정에 실패하였습니다.'));
+        createCommand();
     }
     if (message.t === 'INTERACTION_CREATE') {
         try {
@@ -99,6 +31,78 @@ export async function handler(message: Message) {
             console.error('Error created by' + e);
         }
     }
+}
+
+function createCommand() {
+    createGlobalCommand({
+        name: 'list',
+        description: '추가한 디시콘 목록을 확인합니다.',
+        type: 1,
+    })
+        .then((res) => {
+            if (res?.ok !== true) {
+                console.log(res?.message);
+                return;
+            }
+            console.log('명령어 설정 완료');
+        })
+        .catch(() => console.error('명령어 설정에 실패하였습니다.'));
+    createGlobalCommand({
+        name: 'select',
+        description: '특정 디시콘을 idx, selector를 사용해서 전송합니다.',
+        type: 1,
+        options: [
+            {
+                type: 3,
+                name: 'idx',
+                description: '해당 디시콘의 idx 값을 넣어주세요',
+            },
+            {
+                type: 4,
+                name: 'selector',
+                description: '몇번째 콘을 가져올건지 입력 해 주세요',
+                min_value: 1,
+                max_value: 100,
+            },
+        ],
+    })
+        .then((res) => {
+            if (res?.ok !== true) {
+                console.log(res?.message);
+                return;
+            }
+            console.log('명령어 설정 완료');
+        })
+        .catch(() => console.error('명령어 설정에 실패하였습니다.'));
+    createGlobalCommand({
+        name: 'profile',
+        description: '프로필에 추가한 디시콘을 index, selector를 사용해서 전송합니다.',
+        type: 1,
+        options: [
+            {
+                type: 4,
+                name: 'index',
+                description: '몇번째 디시콘을 선택할건지 입력 해 주세요',
+                min_value: 1,
+                max_value: 25,
+            },
+            {
+                type: 4,
+                name: 'selector',
+                description: '몇번째 콘을 가져올건지 입력 해 주세요',
+                min_value: 1,
+                max_value: 100,
+            },
+        ],
+    })
+        .then((res) => {
+            if (res?.ok !== true) {
+                console.log(res?.message);
+                return;
+            }
+            console.log('명령어 설정 완료');
+        })
+        .catch(() => console.error('명령어 설정에 실패하였습니다.'));
 }
 
 async function handleInteraction(message: Message) {
@@ -185,19 +189,29 @@ async function selectProfile(
     avatar_link: string,
     application_id: string,
 ) {
-    if (index === 0 || selector === 0) {
-        await removeInteractionResponse(application_id, interaction_token);
-        return;
-    }
-    const List: string[] = await getList(user_id);
-    const idx = List[index - 1];
-    if (idx === undefined) {
+    try {
+        if (index === 0 || selector === 0) {
+            await removeInteractionResponse(application_id, interaction_token);
+            return;
+        }
+        const List: string[] = await getList(user_id);
+        const idx = List[index - 1];
+        if (idx === undefined) {
+            await editInteractionResponse(application_id, interaction_token, {
+                content: '해당하는 디시콘이 없습니다.',
+            });
+            return;
+        }
+        await sendDCcon(idx, selector, interaction_token, user_name, avatar_link, application_id);
+    } catch (e: unknown) {
         await editInteractionResponse(application_id, interaction_token, {
-            content: '해당하는 디시콘이 없습니다.',
+            content: '에러가 발생하였습니다.',
         });
-        return;
+        console.error(e);
+        if (e instanceof Error) {
+            /* empty */
+        }
     }
-    await sendDCcon(idx, selector, interaction_token, user_name, avatar_link, application_id);
 }
 
 async function sendDCcon(
@@ -208,39 +222,49 @@ async function sendDCcon(
     avatar_link: string,
     application_id: string,
 ) {
-    if (idx === '' || selector === 0) {
-        await removeInteractionResponse(application_id, interaction_token);
-        return;
-    }
-    const temp = await fetch('http://localhost:3000/api/info', {
-        method: 'POST',
-        body: JSON.stringify({ idx: idx }),
-        headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+        if (idx === '' || selector === 0) {
+            await removeInteractionResponse(application_id, interaction_token);
+            return;
+        }
+        const temp = await fetch('http://localhost:3000/api/info', {
+            method: 'POST',
+            body: JSON.stringify({ idx: idx }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    const info = (await temp.json()) as {
-        title: string;
-        description: string;
-        main_img: string;
-        idx: string;
-        path: { addr: string; ext: string }[];
-    };
-    const path = info.path[selector - 1];
-    const url = `http://localhost:3000/api/img?u=${path.addr}&e=${path.ext}`;
-    const img_temp = await fetch(url);
-    const image: File = new File([await img_temp.blob()], 'main_image.' + path.ext);
-    const embed = {
-        author: {
-            name: user_name,
-            icon_url: avatar_link,
-        },
-        image: {
-            url: `attachment://main_image.${path.ext}`,
-            width: 200,
-            height: 200,
-        },
-    };
-    await editInteractionResponseFile(application_id, interaction_token, {}, [image]);
+        const info = (await temp.json()) as {
+            title: string;
+            description: string;
+            main_img: string;
+            idx: string;
+            path: { addr: string; ext: string }[];
+        };
+        const path = info.path[selector - 1];
+        const url = `http://localhost:3000/api/img?u=${path.addr}&e=${path.ext}`;
+        const img_temp = await fetch(url);
+        const image: File = new File([await img_temp.blob()], 'main_image.' + path.ext);
+        const embed = {
+            author: {
+                name: user_name,
+                icon_url: avatar_link,
+            },
+            image: {
+                url: `attachment://main_image.${path.ext}`,
+                width: 200,
+                height: 200,
+            },
+        };
+        await editInteractionResponseFile(application_id, interaction_token, {}, [image]);
+    } catch (e: unknown) {
+        await editInteractionResponse(application_id, interaction_token, {
+            content: '해당하는 디시콘이 없거나 전송 중 오류가 발생하였습니다.',
+        });
+        console.error(e);
+        if (e instanceof Error) {
+            /* empty */
+        }
+    }
 }
 
 async function sendList(
@@ -250,46 +274,55 @@ async function sendList(
     avatar_link: string,
     application_id: string,
 ) {
-    if (user_id === '' || interaction_token === '') throw Error('Field is empty');
-    const List: string[] = await getList(user_id);
-    const info = (await Promise.all(
-        List.map(async (item) => {
-            const res = await fetch('http://localhost:3000/api/info', {
-                method: 'POST',
-                body: JSON.stringify({ idx: item }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            if (!res.ok) throw new Error('failed to fetch list');
-            return res.json();
-        }),
-    )) as {
-        title: string;
-        description: string;
-        main_img: string;
-        idx: string;
-        path: string[];
-    }[];
-    const fields = info.map((item) => {
-        return {
-            name: item.title,
-            value:
-                item.description.length > 25
-                    ? item.description.slice(0, 25) + '...'
-                    : item.description,
-            inline: false,
-        };
-    });
-    const result = await editInteractionResponse(application_id, interaction_token, {
-        embeds: [
-            {
-                description: '추가한 디시콘 목록을 확인합니다.',
-                author: {
-                    name: user_name,
-                    icon_url: avatar_link,
+    try {
+        if (user_id === '' || interaction_token === '') throw Error('Field is empty');
+        const List: string[] = await getList(user_id);
+        const info = (await Promise.all(
+            List.map(async (item) => {
+                const res = await fetch('http://localhost:3000/api/info', {
+                    method: 'POST',
+                    body: JSON.stringify({ idx: item }),
+                    headers: { 'Content-Type': 'application/json' },
+                });
+                if (!res.ok) throw new Error('failed to fetch list');
+                return res.json();
+            }),
+        )) as {
+            title: string;
+            description: string;
+            main_img: string;
+            idx: string;
+            path: string[];
+        }[];
+        const fields = info.map((item) => {
+            return {
+                name: item.title,
+                value:
+                    item.description.length > 25
+                        ? item.description.slice(0, 25) + '...'
+                        : item.description,
+                inline: false,
+            };
+        });
+        await editInteractionResponse(application_id, interaction_token, {
+            embeds: [
+                {
+                    description: '추가한 디시콘 목록을 확인합니다.',
+                    author: {
+                        name: user_name,
+                        icon_url: avatar_link,
+                    },
+                    fields: fields,
                 },
-                fields: fields,
-            },
-        ],
-    });
-    console.log(result?.message);
+            ],
+        });
+    } catch (e: unknown) {
+        await editInteractionResponse(application_id, interaction_token, {
+            content: '에러가 발생하였습니다.',
+        });
+        console.error(e);
+        if (e instanceof Error) {
+            /* empty */
+        }
+    }
 }
